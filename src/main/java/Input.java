6b8ea132fc355784146da9498279bc3bbb2bd1d0
HEDO <hedo@oocl.com>
2024-11-20 14:15:10 +0800

    code review add todo

--- src/main/java/Input.java
+++ src/main/java/Input.java
@@ -1,13 +1,15 @@
 public class Input {
+    // todo rename filed
     private String value;
     private int count;
 
+    // todo rename args name
     public Input(String w, int i){
         this.value =w;
         this.count =i;
     }
 
-
+    // todo add setter
     public String getValue() {
         return this.value;
     }
--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -8,13 +8,16 @@ import java.io.CharArrayWriter;
 import java.time.LocalDateTime;
 
 public class WordFrequencyGame {
+    // todo rename getWordFrequency to getResult
     public String getResult(String inputStr){
 
 
+        // TODO if else 块
+        // todo 抽取 \\s+ 为常量
         if (inputStr.split("\\s+").length==1) {
             return inputStr + " 1";
         } else {
-
+            // todo 抽出多个方法
             try {
 
                 //split the input string with 1 to n pieces of spaces
@@ -26,6 +29,7 @@ public class WordFrequencyGame {
                     inputList.add(input);
                 }
 
+                // todo 方法名不够清晰，清晰方法名可以不用注释
                 //get the map for the next step of sizing the same word
                 Map<String, List<Input>> map =getListMap(inputList);
 
@@ -53,6 +57,8 @@ public class WordFrequencyGame {
     }
 
 
+    // todo use stream api to replace the for loop
+    // refactor this method
     private Map<String,List<Input>> getListMap(List<Input> inputList) {
         Map<String, List<Input>> map = new HashMap<>();
         for (Input input :  inputList){
