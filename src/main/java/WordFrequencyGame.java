634450ace5cb2612bb6e2a23db80032e657c0a24
HEDO <hedo@oocl.com>
2024-11-20 14:34:19 +0800

    refactor: rename

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -5,40 +5,38 @@ import java.util.Map;
 import java.util.StringJoiner;
 
 public class WordFrequencyGame {
-    // todo rename getWordFrequency to getResult
-    public String getResult(String inputStr) {
+    public String getWordFrequency(String sentence) {
         // TODO if else 块
         // todo 抽取 \\s+ 为常量
-        if (inputStr.split("\\s+").length == 1) {
-            return inputStr + " 1";
+        if (sentence.split("\\s+").length == 1) {
+            return sentence + " 1";
         } else {
             // todo 抽出多个方法
             try {
                 //split the input string with 1 to n pieces of spaces
-                String[] arr = inputStr.split("\\s+");
+                String[] words = sentence.split("\\s+");
 
-                List<Input> inputList = new ArrayList<>();
-                for (String s : arr) {
-                    Input input = new Input(s, 1);
-                    inputList.add(input);
+                List<WordFrequency> wordFrequencies = new ArrayList<>();
+                for (String word : words) {
+                    WordFrequency wordFrequency = new WordFrequency(word, 1);
+                    wordFrequencies.add(wordFrequency);
                 }
 
-                // todo 方法名不够清晰，清晰方法名可以不用注释
-                //get the map for the next step of sizing the same word
-                Map<String, List<Input>> map = getListMap(inputList);
+                //get the wordFrequencyMap for the next step of sizing the same word
+                Map<String, List<WordFrequency>> wordFrequencyMap = getWordFrequencyMap(wordFrequencies);
 
-                List<Input> list = new ArrayList<>();
-                for (Map.Entry<String, List<Input>> entry : map.entrySet()) {
-                    Input input = new Input(entry.getKey(), entry.getValue().size());
-                    list.add(input);
+                List<WordFrequency> tempList = new ArrayList<>();
+                for (Map.Entry<String, List<WordFrequency>> entry : wordFrequencyMap.entrySet()) {
+                    WordFrequency wordFrequency = new WordFrequency(entry.getKey(), entry.getValue().size());
+                    tempList.add(wordFrequency);
                 }
-                inputList = list;
+                wordFrequencies = tempList;
 
-                inputList.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
+                wordFrequencies.sort((w1, w2) -> w2.getWordCount() - w1.getWordCount());
 
                 StringJoiner joiner = new StringJoiner("\n");
-                for (Input w : inputList) {
-                    String s = w.getValue() + " " + w.getWordCount();
+                for (WordFrequency w : wordFrequencies) {
+                    String s = w.getWord() + " " + w.getWordCount();
                     joiner.add(s);
                 }
                 return joiner.toString();
@@ -50,18 +48,18 @@ public class WordFrequencyGame {
 
     // todo use stream api to replace the for loop
     // refactor this method
-    private Map<String, List<Input>> getListMap(List<Input> inputList) {
-        Map<String, List<Input>> map = new HashMap<>();
-        for (Input input : inputList) {
-//       map.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
-            if (!map.containsKey(input.getValue())) {
-                ArrayList arr = new ArrayList<>();
-                arr.add(input);
-                map.put(input.getValue(), arr);
+    private Map<String, List<WordFrequency>> getWordFrequencyMap(List<WordFrequency> wordFrequencyList) {
+        Map<String, List<WordFrequency>> wordFrequencyMap = new HashMap<>();
+        for (WordFrequency wordFrequency : wordFrequencyList) {
+//       wordFrequencyMap.computeIfAbsent(input.getValue(), k -> new ArrayList<>()).add(input);
+            if (!wordFrequencyMap.containsKey(wordFrequency.getWord())) {
+                ArrayList tempList = new ArrayList<>();
+                tempList.add(wordFrequency);
+                wordFrequencyMap.put(wordFrequency.getWord(), tempList);
             } else {
-                map.get(input.getValue()).add(input);
+                wordFrequencyMap.get(wordFrequency.getWord()).add(wordFrequency);
             }
         }
-        return map;
+        return wordFrequencyMap;
     }
 }
