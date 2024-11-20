4fcfff8daeffad8e371872963953df246feb0bc1
HEDO <hedo@oocl.com>
2024-11-20 15:38:19 +0800

    refactor: magic word

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -3,13 +3,14 @@ import java.util.stream.Collectors;
 
 public class WordFrequencyGame {
 
-    public static final String SPACE = "\\s+";
+    public static final String SPACE_REGEX = "\\s+";
     public static final String LINE_BREAK = "\n";
     public static final String CALCULATE_ERROR = "Calculate Error";
+    public static final String SPACE = " ";
 
     public String getWordFrequency(String sentence) {
         // TODO if else 块
-        if (sentence.split(SPACE).length == 1) {
+        if (sentence.split(SPACE_REGEX).length == 1) {
             return sentence + " 1";
         } else {
             try {
@@ -25,7 +26,7 @@ public class WordFrequencyGame {
 
     private String buildResult(List<WordFrequency> wordFrequencies) {
         return wordFrequencies.stream()
-                .map(wordFrequency -> wordFrequency.getWord() + " " + wordFrequency.getWordCount())
+                .map(wordFrequency -> wordFrequency.getWord() + SPACE + wordFrequency.getWordCount())
                 .collect(Collectors.joining(LINE_BREAK));
     }
 
@@ -40,7 +41,7 @@ public class WordFrequencyGame {
 
     private List<WordFrequency> getInitialWordFrequencies(String sentence) {
         //split the input string with 1 to n pieces of spaces
-        String[] words = sentence.split(SPACE);
+        String[] words = sentence.split(SPACE_REGEX);
         return Arrays.stream(words)
                 .map(word -> new WordFrequency(word, 1))
                 .toList();
