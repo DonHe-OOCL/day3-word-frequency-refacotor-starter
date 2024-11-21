647f31bb3e5144afad9fc8cad32993b680543e26
HEDO <hedo@oocl.com>
2024-11-21 08:59:36 +0800

    refactor: use stream API reduce listToMap method

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -30,12 +30,11 @@ public class WordFrequencyGame {
     }
 
     private List<WordFrequency> getWordFrequencies(List<WordFrequency> wordFrequencies) {
-        Map<String, List<WordFrequency>> wordToWordFrequencies = getWordFrequencyMap(wordFrequencies);
-
-        return wordToWordFrequencies.entrySet().stream()
+        return wordFrequencies.stream().collect(Collectors.groupingBy(WordFrequency::getWord))
+                .entrySet().stream()
                 .map(entry -> new WordFrequency(entry.getKey(), entry.getValue().size()))
                 .sorted((word, nextWord) -> nextWord.getWordCount() - word.getWordCount())
-                .collect(Collectors.toList());
+                .toList();
     }
 
     private List<WordFrequency> getInitialWordFrequencies(String sentence) {
@@ -44,10 +43,5 @@ public class WordFrequencyGame {
         return Arrays.stream(words)
                 .map(word -> new WordFrequency(word, 1))
                 .toList();
-    }
-
-    // refactor this method
-    private Map<String, List<WordFrequency>> getWordFrequencyMap(List<WordFrequency> wordFrequencyList) {
-        return wordFrequencyList.stream().collect(Collectors.groupingBy(WordFrequency::getWord));
     }
 }
